c8e5ec620bf34075caee91ca78ddf82772a32eea
tcdat96 <tcdat96@gmail.com>
2020-05-30 00:52:15 -0500

    update notification message + prompt user to hide the nofication

--- app/src/main/java/com/todaystudio/soha/utils/NotificationUtil.kt
+++ app/src/main/java/com/todaystudio/soha/utils/NotificationUtil.kt
@@ -2,21 +2,30 @@ import android.app.*
 import android.content.Context
 import android.content.Intent
 import android.os.Build
+import android.provider.Settings
 import androidx.core.app.NotificationCompat
 import com.todaystudio.soha.MainActivity
 import com.todaystudio.soha.R
 
 object NotificationUtil {
     fun getNotification(context: Context, channelId: String): Notification {
-        val pendingIntent: PendingIntent =
-            Intent(context, MainActivity::class.java).let { notificationIntent ->
-                PendingIntent.getActivity(context, 0, notificationIntent, 0)
-            }
-
         createNotificationChannel(context, channelId)
 
+        // prompt user to hide the notification
+        val intent = Intent().apply {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                action = Settings.ACTION_APP_NOTIFICATION_SETTINGS
+                putExtra(Settings.EXTRA_APP_PACKAGE, context.packageName)
+            } else {
+                action = "android.settings.APP_NOTIFICATION_SETTINGS"
+                putExtra("app_package", context.packageName)
+                putExtra("app_uid", context.applicationInfo.uid)
+            }
+        }
+        val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
+
         return NotificationCompat.Builder(context, channelId)
-            .setContentTitle(context.getText(R.string.app_name))
+            .setContentTitle(context.getText(R.string.notification_title))
             .setContentText(context.getText(R.string.notification_message))
             .setSmallIcon(R.drawable.ic_launcher_foreground)
             .setContentIntent(pendingIntent)
